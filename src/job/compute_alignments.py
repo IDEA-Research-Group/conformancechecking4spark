9941b192f710a490a809cd462c9eec52ab00c296
Alvaro <avalenciaparra@gmail.com>
2020-11-17 13:14:44 +0100

    feat: implemente map and reduceByKey functions

--- src/job/compute_alignments.py
+++ src/job/compute_alignments.py
@@ -1,34 +1,73 @@
+import math
+from pm4py.algo.conformance.alignments import algorithm as alignments
 
 
 class DistributedAlignmentProblem:
-
     """
     The following parameters must be defined:
     :param SparkContext
     
     """
 
-    # Define several constructors
+    def __init__(self, log_rdd, pm_rdd, log_slices, pm_slices, global_timeout=None, trace_timeout=None,
+                 algorithm=None, heuristic=None):
+        #self._sc = sc
+        self._log_rdd = log_rdd
+        self._pm_rdd = pm_rdd
+        self._log_slices = log_slices
+        self._pm_slices = pm_slices
+        self._global_timeout = global_timeout
+        self._trace_timeout = trace_timeout
+        self._algorithm = algorithm
+        self._heuristic = heuristic
 
+    def _process_partition(self, iterator):
+        partial_solutions = {}
+        estimations = []
 
+        for element in iterator:
+            trace = element[0]
+            model, initial_marking, final_marking = element[1]
+            estimation = self._heuristic(trace, model)
+            estimations.append((element[0], element[1], estimation))  # (trace, model, estimation)
 
-    def __init__(self, sc, trace_rdd, pm_rdd, log_slices, pm_slices, global_timeout=None, trace_timeout=None, algorithm=None, heuristic=None):
-        self._pm_rdd = None
-        self._log_rdd = None
-        self._sc = None
+        estimations.sort(key=(lambda x: x[2]))  # sort by estimation
 
-    def _init_spark(self):
-        pass
+        for element in estimations:
+            trace = element[0]
+            model, initial_marking, final_marking = element[1]
+
+            trace_name = trace.attributes['concept:name']
+            prev_cost = math.inf
+
+            if trace_name in partial_solutions.keys():
+                prev_cost = partial_solutions[trace_name]
+                estimation = element[2]
+                if prev_cost <= estimation:  # if previous cost is better than estimation, brak
+                    break
+                if prev_cost == 0:  # if prev_cost is 0, break
+                    break
+
+            alignment = alignments.apply_trace(trace, model, initial_marking, final_marking)
+            cost = int(alignment['cost'] / 10000)
+
+            if cost < prev_cost:
+                partial_solutions[trace_name] = cost
+
+        return [(k, v) for k, v in partial_solutions.items()]
+
+    def _reduce_partitions(self, ps1, ps2):
+        return min(ps1, ps2)
 
     def run(self):
-        sc = self._sc
-        log_rdd = self._log_rdd
-        pm_rdd = self._pm_rdd
-        
-        
+        partitions = self._log_rdd.repartition(self._log_slices).cartesian(self._pm_rdd.repartition(self._pm_slices))
+
+        partitions.mapPartitions(self._process_partition) \
+            .reduceByKey(self._reduce_partitions) \
+            .show()
         pass
 
-    builder = Build()
+    # builder = Build()
 
     class Build:
         """
@@ -76,10 +115,5 @@ class DistributedAlignmentProblem:
             self._memory_driver = None
             pass
 
-
         def build(self):
-
-
-
-
-
+            pass
