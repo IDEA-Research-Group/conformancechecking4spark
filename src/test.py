5575c68f7c1cc8285947b0f739b04c49a523573f
Alvaro <avalenciaparra@gmail.com>
2020-11-18 12:56:26 +0100

    chores: added test.py for testing purposes

---
+++ src/test.py
@@ -0,0 +1,23 @@
+from pyspark import SparkContext, SparkConf
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.objects.petri.importer import importer as pnml_importer
+import config
+import os
+from src.job.compute_alignments import DistributedAlignmentProblem
+from src.heuristics import calculate_min_cost
+
+conf = SparkConf().setAppName("test").setMaster("local[*]")
+sc = SparkContext(conf=conf)
+
+log = xes_importer.apply(os.path.join(config.ROOT_DIR, 'data/M2.xes'))
+net, initial_marking, final_marking = pnml_importer.apply(os.path.join(config.ROOT_DIR, 'data/M2_petri_pnml.pnml'))
+
+log_rdd = sc.parallelize(log)
+pm_rdd = sc.parallelize([(net, initial_marking, final_marking)])
+
+distr_alg = DistributedAlignmentProblem(log_rdd, pm_rdd, 5, 1, heuristic=calculate_min_cost)
+
+distr_alg.apply()
+
+
+
