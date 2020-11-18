2957cd6ba4be6a34c513ba0bcb29959cfc3b6e8e
Alvaro <avalenciaparra@gmail.com>
2020-11-18 16:33:08 +0100

    refactor: created heuristics package for the heuristic algorithms and utility functions

--- src/heuristics.py
+++
@@ -1,6 +0,0 @@
-
-from src.utils import sum_dif_dict, get_repeated_log, get_repeated_net
-
-
-def calculate_min_cost(log, net):
-    return sum_dif_dict(get_repeated_log(log), get_repeated_net(net))
---
+++ src/heuristics/algorithm.py
@@ -0,0 +1,6 @@
+
+from src.heuristics.utils import sum_dif_dict, get_repeated_log, get_repeated_net
+
+
+def sum_of_differences(log, net):
+    return sum_dif_dict(get_repeated_log(log), get_repeated_net(net))
