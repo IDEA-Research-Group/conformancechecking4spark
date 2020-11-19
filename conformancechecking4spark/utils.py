f1f2f7095d0a4c06b0e345e8d7fbb25ca9489c45
Alvaro <avalenciaparra@gmail.com>
2020-11-19 13:18:39 +0100

    refactor: created examples directory for quick testing. Added remove_hdfs_directory to utils.py

--- conformancechecking4spark/utils.py
+++ conformancechecking4spark/utils.py
@@ -1,6 +1,12 @@
 from os.path import isfile, join
 from os import listdir
+from hdfs import InsecureClient
 
 
 def get_partial_models(directory):
     return [f for f in listdir(directory) if isfile(join(directory, f))]
+
+
+def remove_hdfs_directory(path, server, user, http_port=50070):
+    client = InsecureClient(url="http://{0}:{1}".format(server, http_port), user=user)
+    return client.delete(path, recursive=True)
