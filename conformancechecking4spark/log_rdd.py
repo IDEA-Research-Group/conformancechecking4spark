f1391667ca1e243acfcd84ce6b49a6803aebbd3e
Alvaro Valencia <alvarovalenciavp@gmail.com>
2021-06-17 19:25:54 +0200

    refactor: structured xml parsers in a new module

--- conformancechecking4spark/log_rdd.py
+++ conformancechecking4spark/log_rdd.py
@@ -1,16 +1,21 @@
-from pm4py.objects.log.obj import Trace, Event
-from pm4py.util.dt_parsing import parser as dt_parser
-from pyspark import Row
+from conformancechecking4spark.log_parse_utils.from_xml import parse_xml_row
 
 
-def create_log_rdd_from_xes(sc, path, slices=None):
+def create_from_xes(spark_session, path):
     """
-    Requires config("spark.jars.packages", "com.databricks:spark-xml_2.12:0.12.0")
+    Requires config("spark.jars.packages", "com.databricks:spark-xml_2.12:0.12.0") in spark config
     :param sc:
     :param path:
-    :param slices:
     :return:
     """
+    df = spark_session.read \
+        .format("com.databricks.spark.xml") \
+        .option("rootTag", "log") \
+        .option("rowTag", "trace") \
+        .option("inferSchema", "false") \
+        .load(path)
+
+    return format_xml_rdd(df.rdd)
 
 
 def format_xml_rdd(rdd):
@@ -23,6 +28,14 @@ def format_xml_rdd(rdd):
     return rdd.map(lambda r: parse_xml_row(r))
 
 
+def create_from_csv():
+    pass
+
+
+# Group by trace => format
+def format_csv_rdd():
+    pass
+
 
 
 
@@ -31,49 +44,7 @@ def format_xml_rdd(rdd):
 
 # UTILS
 
-def extract_key_and_value_from_xml(d):
-    if "_key" in d.keys():
-        if "_value" in d.keys():
-            return {d["_key"]: d["_value"]}
-        else:
-            return {d["_key"]: None}
 
-
-def xml_row_to_dict(row):
-    d = {}
-    # first level groups attributes by tag name. For example, if there is one attribute in a <date> tag,
-    # and two attributes in two <string> tags, then there will be one element inside the "date" row attribute,
-    # and a list with two elements inside the "string" row attribute
-    for element in row:
-        # if there is more than one element witht his type of tag
-        if type(element) == list:
-            for att in element:
-                d.update(extract_key_and_value_from_xml(att.asDict()))
-        else:
-            d.update(extract_key_and_value_from_xml(element.asDict()))
-    return d
-
-
-def from_dict_to_event(event_dict):
-    timestamp_field_name = "time:timestamp"
-    if timestamp_field_name in event_dict.keys():
-        event_dict[timestamp_field_name] = dt_parser.get().apply(event_dict[timestamp_field_name])
-    return Event(event_dict)
-
-
-def from_dicts_to_trace(event_dicts, trace_info_dict):
-    events = [from_dict_to_event(ed) for ed in event_dicts]
-    return Trace(events, attributes=trace_info_dict)
-
-
-def parse_xml_row(row: Row):
-    # it must contain at least one field called event, containing a list of elements under the parent tag <event></event>
-    events = [xml_row_to_dict(r) for r in row["event"]]
-    # the rest of the row represent the information on the trace
-    r_dict = row.asDict()
-    trace_info_raw = [r_dict[i] for i in r_dict.keys() if i != "event"]
-    trace_info = xml_row_to_dict(trace_info_raw)
-    return from_dicts_to_trace(events, trace_info)
 
 
 
