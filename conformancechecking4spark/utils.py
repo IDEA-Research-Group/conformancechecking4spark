8802ebc1d5eab2d75ed0ed839d0b42cae0d177a0
Alvaro Valencia <alvarovalenciavp@gmail.com>
2021-06-18 12:40:22 +0200

    refactor: added new function to utils.py to create default spark session

--- conformancechecking4spark/utils.py
+++ conformancechecking4spark/utils.py
@@ -2,6 +2,7 @@ from os.path import isfile, join
 from os import listdir
 from hdfs import InsecureClient
 from pm4py.objects.petri.importer import importer as pnml_importer
+from pyspark.sql import SparkSession
 
 
 def get_partial_models(directory):
@@ -11,3 +12,12 @@ def get_partial_models(directory):
 def remove_hdfs_directory(path, server, user, http_port=50070):
     client = InsecureClient(url="http://{0}:{1}".format(server, http_port), user=user)
     return client.delete(path, recursive=True)
+
+
+def create_default_spark_session():
+    return SparkSession.builder \
+        .appName("pm4py_test") \
+        .master("local[*]") \
+        .config("spark.jars.packages", "com.databricks:spark-xml_2.12:0.12.0") \
+        .getOrCreate()
+
