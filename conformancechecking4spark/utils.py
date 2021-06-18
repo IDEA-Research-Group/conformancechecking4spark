c37b48e91dfc468a0749ef7ea6d183631071df11
Alvaro Valencia <alvarovalenciavp@gmail.com>
2021-06-18 19:23:23 +0200

    refactor: hdfs client dependency. HDFS directory removal feature has been deleted

--- conformancechecking4spark/utils.py
+++ conformancechecking4spark/utils.py
@@ -1,17 +1,11 @@
 from os.path import isfile, join
 from os import listdir
-from hdfs import InsecureClient
 from pm4py.objects.petri.importer import importer as pnml_importer
 from pyspark.sql import SparkSession
 
 
 def get_partial_models(directory):
     return [pnml_importer.apply(join(directory, f)) for f in listdir(directory) if isfile(join(directory, f))]
-
-
-def remove_hdfs_directory(path, server, user, http_port=50070):
-    client = InsecureClient(url="http://{0}:{1}".format(server, http_port), user=user)
-    return client.delete(path, recursive=True)
 
 
 def create_default_spark_session():
